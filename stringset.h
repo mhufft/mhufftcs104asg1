20e81f17a66b64e02eef9abe5abe2560dff11d67
Maximillian Thaddeus Hufft <mhufft@unix1.lt.ucsc.edu>
2014-10-19 13:58:05 -0700

    Updated stringset.cpp stringset.h

--- stringset.h
+++ stringset.h
@@ -1,1 +1,17 @@
+//Max Hufft and Carl Eadler
+//mhufft@ucsc.edu ceadler@ucsc.edu
 
+//stringset.h
+//Starter code from Westly Mackey
+
+#ifndef __STRINGSET__
+#define __STRINGSET__
+
+#include <iostream>
+#include <string>
+
+const std::string* intern_stringset (const char*);
+
+void dump_stringset (std::ostream&);
+
+#endif
